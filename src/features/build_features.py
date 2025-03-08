75d83a045091595c4bb9f299998ac56de1a4bc65
faizanSayyed7 <sayyedfaizan9558@gmail.com>
2025-03-08 07:59:55 +0530

    applies butterWorth Filter

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -36,18 +36,49 @@ df[df["set"] == 25]["acc_y"].plot()
 df[df["set"] == 6]["acc_y"].plot()
 
 duration = df[df["set"] == 1].index[-1] - df[df["set"] == 1].index[0]
-
 duration.seconds
 
-for s in sorted(df["set"].unique()):
-    print(s)
+for s in df["set"].unique():
+    start = df[df["set"] == s].index[0]
+    stop = df[df["set"] == s].index[-1]
 
+    duration = stop - start
+
+    df.loc[(df["set"] == s), "duration"] = duration.seconds
+
+
+duration_df = df.groupby(["category"])["duration"].mean()
+
+duration_df.iloc[0] / 5
+duration_df.iloc[1] / 10
 
 # --------------------------------------------------------------
 # Butterworth lowpass filter
 # --------------------------------------------------------------
 
+df_lowpass = df.copy()
+LowPass = LowPassFilter()
 
+fs = 1000 / 200
+
+cutoff = 1.3
+
+df_lowpass = LowPass.low_pass_filter(df_lowpass, "acc_y", fs, cutoff, order=5)
+
+subset = df_lowpass[df_lowpass["set"] == 45]
+print(subset["label"][0])
+
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20,10))
+ax[0].plot(subset["acc_y"].reset_index(drop=True), label="raw data")
+ax[1].plot(subset["acc_y_lowpass"].reset_index(drop=True), label="butterWorth Filter")
+ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), fancy_box=True, shadow=True)
+ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), fancy_box=True, shadow=True)
+
+for col in predictor_columns:
+    df_lowpass = LowPass.low_pass_filter(df_lowpass, col, fs, cutoff, order=5)
+    df_lowpass[col] = df_lowpass[col + "_lowpass"]
+    del df_lowpass[col + "_lowpass" ]
+    
 # --------------------------------------------------------------
 # Principal component analysis PCA
 # --------------------------------------------------------------
